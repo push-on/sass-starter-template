e7e2be95366ef4fa747a3b8098efd42e597e14b8
mhimranhossain <mh.imranhossain@outlook.com>
2022-12-06 19:49:08 +0600

    cleanup

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
   plugins: [
     purgecss({
-      content: ['./**/*.html', './**/*.svelte', './**/*.js', './**/*.ts', './**/*.jsx', './**/*.tsx']
+      content: ['./**/*.html', './**/*.svelte']
     })
   ]
 }
