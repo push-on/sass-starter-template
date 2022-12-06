10dd9b91ec6281ab159356b20dbc98ebe7cee5f6
mhimranhossain <mh.imranhossain@outlook.com>
2022-12-06 13:08:49 +0600

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
-<main class="container">
-  <h1 class="underline">hello World</h1>
-  <input type="text">
-  <button>Button</button>
+<script>
+  import Page from './libs/page.svelte'
+</script>
+
+<main>
+  <Page />
 </main>
